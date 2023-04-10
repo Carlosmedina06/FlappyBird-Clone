0ac82d600591652a653b1a2adb5fc62c232651f8
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-10 18:53:02 -0300

    imagen de fondo y detalles del score temporales

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
-import { StatusBar, Text, TouchableOpacity, View } from 'react-native'
+import { StatusBar, Text, TouchableOpacity, View, ImageBackground, Image } from 'react-native'
 import { GameEngine } from 'react-native-game-engine'
 
 import entities from './src/entities'
 import Physics from './src/physics'
+import background from './src/assets/background.png'
+import gameOver from './src/assets/gameover.png'
 
 export default function App() {
   const [running, setRunning] = useState(false)
@@ -15,59 +17,73 @@ export default function App() {
   }, [])
 
   return (
-    <View style={{ flex: 1 }}>
-      <Text
-        style={{
-          textAlign: 'center',
-          fontSize: 40,
-          fontWeight: 'bold',
-          margin: 20,
-          zIndex: 2,
-          top: 25,
-        }}
-      >
-        {currentPoints}
-      </Text>
-      <GameEngine
-        ref={(ref) => {
-          setGameEngine(ref)
-        }}
-        entities={entities()}
-        running={running}
-        style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
-        systems={[Physics]}
-        onEvent={(e) => {
-          switch (e.type) {
-            case 'game_over':
-              setRunning(false)
-              gameEngine.stop()
-              break
-            case 'score':
-              setCurrentPoints(currentPoints + 1)
-              break
-          }
-        }}
-      >
-        <StatusBar hidden={true} />
-      </GameEngine>
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <ImageBackground source={background} style={{ width: '100%', height: '100%' }}>
+        <Text
+          style={{
+            backgroundColor: 'rgba(0,0,0,0.5)',
+            borderRadius: 10,
+            paddingHorizontal: 30,
+            paddingVertical: 10,
+            zIndex: 1,
+            position: 'absolute',
+            top: 50,
+            left: '40%',
+            fontWeight: 'bold',
+            color: 'white',
+            fontSize: 30,
+          }}
+        >
+          {currentPoints}
+        </Text>
+        <GameEngine
+          ref={(ref) => {
+            setGameEngine(ref)
+          }}
+          entities={entities()}
+          running={running}
+          style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+          systems={[Physics]}
+          onEvent={(e) => {
+            switch (e.type) {
+              case 'game_over':
+                setRunning(false)
+                gameEngine.stop()
+                break
+              case 'score':
+                setCurrentPoints(currentPoints + 1)
+                break
+            }
+          }}
+        >
+          <StatusBar hidden={true} />
+        </GameEngine>
 
-      {!running ? (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <TouchableOpacity
-            style={{ backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10 }}
-            onPress={() => {
-              setCurrentPoints(0)
-              setRunning(true)
-              gameEngine.swap(entities())
-            }}
-          >
-            <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
-              YOU LOST, POINT: {currentPoints}
-            </Text>
-            <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>RESTART GAME</Text>
-          </TouchableOpacity>
-        </View>
-      ) : null}
+        {!running ? (
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <TouchableOpacity
+              style={{
+                backgroundColor: 'rgba(0,0,0,0.5)',
+                borderRadius: 10,
+                paddingHorizontal: 30,
+                paddingVertical: 10,
+              }}
+              onPress={() => {
+                setCurrentPoints(0)
+                setRunning(true)
+                gameEngine.swap(entities())
+              }}
+            >
+              <Image source={gameOver} />
+              <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>RESTART GAME</Text>
+            </TouchableOpacity>
+          </View>
+        ) : null}
+      </ImageBackground>
     </View>
   )
 }
